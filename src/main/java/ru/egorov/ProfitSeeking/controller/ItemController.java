8a69bea75f6046d96459f94f39aa1feb1b4fc8db
Viktor Egorov <107366919+remowilliamsss@users.noreply.github.com>
2023-03-09 17:19:31 +0300

    added pagination for search results

--- src/main/java/ru/egorov/ProfitSeeking/controller/ItemController.java
+++ src/main/java/ru/egorov/ProfitSeeking/controller/ItemController.java
@@ -1,11 +1,13 @@
 package ru.egorov.ProfitSeeking.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.egorov.ProfitSeeking.model.Item;
 import ru.egorov.ProfitSeeking.model.StoreType;
 import ru.egorov.ProfitSeeking.service.ItemService;
 
@@ -28,8 +30,22 @@ public class ItemController {
     }
 
     @PostMapping("/search")
-    public String search(Model model, @RequestParam("query") String query, Pageable pageable) {
-        model.addAttribute("results", itemService.findAllByName(query, pageable));
+    public String search(@RequestParam("query") String query) {
+        return "redirect:search/" + query + "/1";
+    }
+
+    @GetMapping("/search/{query}/{page}")
+    public String search(@PathVariable("query") String query, @PathVariable("page") Integer page, Model model) {
+        Pageable pageable = PageRequest.of(page - 1, 30);
+
+        model.addAttribute("page", page);
+        model.addAttribute("query", query);
+        model.addAttribute("pagination", List.of(1, 2, 3, 4, 5, 6, 7));
+
+        Page<Item> productPage = itemService.findAllByName(query, pageable);
+
+        model.addAttribute("results", productPage.getContent());
+        model.addAttribute("pageCount", productPage.getTotalPages());
 
         return "search";
     }
